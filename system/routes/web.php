87af777dff59fae45d2770abee4a3fd82c688642
ahmadrhmndni <ahmadaksen123@gmail.com>
2020-10-25 14:04:42 +0700

    Initial commit

--- system/routes/web.php
+++ system/routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index' , function () {
+	return view('home');
+});
+
+Route::get('/login' , function () {
+	return view('login');
+});
+
+Route::get('/reccomended' , function () {
+	return view('rekomendasi');
+});
+
